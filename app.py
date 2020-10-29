88a6fbef6e0150bc2c1a9ca0632763fb0cd57997
phuphan626 <65184304+phuphan626@users.noreply.github.com>
2020-10-29 00:55:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,30 @@
 from flask import Flask, render_template
-from flask_pymongo import PyMongo
-from pymongo import MongoClient
 import Scrape 
-import joblib
 
 app = Flask(__name__)
 
-
-
 @app.route("/") 
 def home():
-   
+
     return render_template("index.html")
+@app.route("/gantt.html") 
+def gantt():
+    return render_template("gantt.html")
+@app.route("/radial.html") 
+def radial():
+    return render_template("radial.html")    
 
 @app.route("/bar.html") 
 def bar():
 
     return render_template("bar.html")
 
-@app.route("/gantt.html") 
-def gantt():
-
-    return render_template("gantt.html")
-
-@app.route("/radial.html") 
-def radial():
-
-    return render_template("radial.html")    
-
 
 @app.route("/scrape") 
 def scrape():
     
     data = Scrape.scrapping() #activates the scrapping function
-    # linear_model = joblib.load('Linear_model.sav')
-    # prediction = linear_model.predict([data])
-    # logistic_model = joblib.load('Logistic_model.sav')
+
     return render_template("scrape.html", xyz = data )
     
 
